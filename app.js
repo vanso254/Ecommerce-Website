72b49ac65240510018526892933e94769bcc61d7
Evans Oduor <oduorevans9@gmail.com>
2022-11-21 12:10:28 +0300

    Fixing the login page

--- app.js
+++ app.js
@@ -15,26 +15,25 @@ const User = require('./models/user')
 //starting app and mongoDb local storage server
 const app = express();
 const store = new MongoDBStore({
-  uri: 'mongodb://localhost:27017/shoppingMongoose',
-  collection: 'sessions'
+    uri: 'mongodb://localhost:27017/shoppingMongoose',
+    collection: 'sessions'
 });
 const csrfProtection = new csrf();
 const fileStorage = multer.diskStorage({
-  destination: (req,file,cb)=>{
-    cb(null, 'images')
-  },
-  filename: (req,file,cb)=>{
-    cb(null,new Date().toISOString() + '-' + file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
 })
-const fileFilterF = (req,file,cb)=>{
-  if((file.mimetype=='image/png')||(file.mimetype=='image/jpg')||(file.mimetype=='image/jpeg')){
-    // console.log('approved format');
-    cb(null,true);
-  }
-  else{
-    cb(null,false);
-  }
+const fileFilterF = (req, file, cb) => {
+    if ((file.mimetype == 'image/png') || (file.mimetype == 'image/jpg') || (file.mimetype == 'image/jpeg')) {
+        // console.log('approved format');
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 }
 
 //setting up dynamic rendering engine
@@ -45,17 +44,19 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+const dashboard = require('./routes/dashboard')
+app.use('/dashboard', dashboard);
 
 //middlewares to be used by every incoming request
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage: fileStorage, fileFilter: fileFilterF}).single('imageUrl')) //configuring file storage options
+app.use(multer({ storage: fileStorage, fileFilter: fileFilterF }).single('imageUrl')) //configuring file storage options
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(session({
-  secret: 'nightwayne', 
-  resave: false, 
-  saveUninitialized: false,
-  store: store
+    secret: 'nightwayne',
+    resave: false,
+    saveUninitialized: false,
+    store: store
 }))
 
 //csrfProtection & flash messages which get destroyed when request served(through response-send/redirect)
@@ -64,21 +65,21 @@ app.use(flash());
 
 //session setting up & adding fields to request body
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 //routing requests & error handling middleware
@@ -88,16 +89,17 @@ app.use(authRoutes);
 app.use(errorController.get500)
 app.use(errorController.get404);
 
-app.use(function(err,req,res,next){
-  res.redirect('/500');
+app.use(function(err, req, res, next) {
+    res.redirect('/500');
 });
 
+
 //connecting with db-local and setting up server
-mongoose.connect('mongodb://localhost:27017/shoppingMongoose', {useNewUrlParser: true})
-.then(result => {
-  console.log('Connected to local server!');
-  app.listen(3000);
-})
-.catch(err => {
-  console.log(err);
-});
+mongoose.connect('mongodb://localhost:27017/shoppingMongoose', { useNewUrlParser: true })
+    .then(result => {
+        console.log('Connected to local server!');
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
